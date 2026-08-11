2643e7e548eb07fdbdee01c1fed397ea1d61a173
Ataullah Behesti <ataullah.behesti@gmail.com>
2026-08-11 18:41:34 +0300

    for loop

--- pythonProject/For loops.py
+++ pythonProject/For loops.py
@@ -1,2 +1,16 @@
+'''
 for i in range(5):
     print(i)
+'''
+'''
+range(stop)
+range(start, stop)
+range(start, stop, step)
+
+But remember our rule: **the stop value is not included**.
+'''
+
+# here 10 is start, 0 is stop and -2 is step in negative direction
+
+for number in range(10, 0, -2):
+    print(number)
