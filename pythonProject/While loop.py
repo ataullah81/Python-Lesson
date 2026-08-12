07b7e14da4240bfefff886ac6b3bd099edb0b83d
Ataullah Behesti <ataullah.behesti@gmail.com>
2026-08-12 17:45:12 +0300

    for and while loop

---
+++ pythonProject/While loop.py
@@ -0,0 +1,20 @@
+
+'''
+number = 1
+while number <= 5:
+    print(number)
+    number +=1
+#----------------------
+number = 2
+while number < 10:
+    print(number)
+    number += 2
+
+'''
+# counting backward
+
+number = 5
+while number > 0:
+    print(number)
+    number -=1
+print("Finished")
