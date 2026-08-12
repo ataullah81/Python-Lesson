5b743636f4424deb25b6b0dc5a8dd203f5bef3b8
Ataullah Behesti <ataullah.behesti@gmail.com>
2026-08-12 16:31:57 +0300

    for and while loop

--- pythonProject/For loops.py
+++ pythonProject/For loops.py
@@ -9,8 +9,27 @@ range(start, stop, step)
 
 But remember our rule: **the stop value is not included**.
 '''
-
+'''
 # here 10 is start, 0 is stop and -2 is step in negative direction
 
 for number in range(10, 0, -2):
     print(number)
+    
+#----------------------------------
+
+for number in range(1,5):
+    result = number * 3
+    print(result)
+#------------------------------------
+total = 0
+for number in range(1,7):
+    if number % 2 == 0:
+        total = total + number
+print(total)
+
+'''#------------------------
+total = 0
+for number in range (1, 7):
+    if number % 2 != 0:
+        total = total + number
+print(total)
