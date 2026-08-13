214fd7badc1d1352c5ac9e86fa18af1ad7b765e5
Ataullah Behesti <ataullah.behesti@gmail.com>
2026-08-13 19:26:00 +0300

    Break and continue

--- pythonProject/While loop.py
+++ pythonProject/While loop.py
@@ -18,7 +18,8 @@ while number > 0:
     print(number)
     number -=1
 print("Finished")
-'''
+
+#-------------------------------
 
 password = input("Enter your password: ")
 
@@ -26,3 +27,43 @@ while password != "python123":
     print("Wrong password!")
     password = input("Try again: ")
 print("Access granted!")
+
+#-----------------
+
+password = input("Enter your password: ")
+
+while password != "AI2026":
+    print("Wrong password.")
+    password = input("Try again: ")
+
+print("Access granted!")
+
+#----------------------------
+
+for number in range(1, 8):
+    print(number)
+
+    if number == 4:
+        break
+
+print("Finished")
+
+#-------------------------
+
+for number in range(1, 8):
+    #print(number)
+
+    if number == 4: # skip 3 and print rest
+    #if number % 2 !=0: # print even numbers because it skip the odd numbers
+    #if number % 2 !=0: # print odd numbers because it skip the even numbers
+        continue # skip iteration when number is = 3 and move to the next one
+    print(number)
+
+'''
+
+for number in range(1,10):
+    if number == 5:
+        continue
+    if number == 9:
+        break
+    print(number)
