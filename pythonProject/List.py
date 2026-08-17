c637f2bbe5d7b8de0d14f7d31a65033fde9aef2f
Ataullah Behesti <ataullah.behesti@gmail.com>
2026-08-17 08:19:35 +0300

    List append,pop,remove

--- pythonProject/List.py
+++ pythonProject/List.py
@@ -3,6 +3,39 @@ fruits = ["Apple","Banana","Mango"]
 for fruit in fruits:
     print(fruit)
 
-'''
+
 animals = ["Cat","Dog","Lion","Tiger"]
 print(animals[3])
+
+# Items append to the list
+cars = ["Toyota", "BMW"]
+
+cars.append("Volvo")
+cars.append("Tesla")
+
+print(cars)
+
+# To print items one under another
+
+animals = ["Cat","Dog","Lion","Tiger"]
+for animal in animals:
+    print(animal)
+
+
+
+cars = ["Toyota", "BMW", "Volvo", "Tesla"]
+cars.remove("BMW")
+print(cars)
+
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+fruits.pop(1)
+print(fruits)
+
+
+# remove item from the list and print removed item and remaining items
+animals = ["Cat", "Dog", "Lion", "Tiger"]
+removed_animals = animals.pop(2)
+print(removed_animals)
+print(animals)
+
+'''
