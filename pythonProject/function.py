86173f44f0f7bdf92cfeb1b46618d009862c34ab
Ataullah Behesti <ataullah.behesti@gmail.com>
2026-08-28 21:22:18 +0300

    Learning function

---
+++ pythonProject/function.py
@@ -0,0 +1,16 @@
+'''
+def welcome():
+    print("Hello world")
+    print("Let's have fun")
+
+
+welcome()
+welcome()
+'''
+
+def student(name, score):
+    print("Name:", name)
+    print("Score:", score)
+
+student("Ali", 85)
+student("Sara", 92)
